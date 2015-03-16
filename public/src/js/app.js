854705392625ff41eb3a8c02464fae6701e0a551
arno yim <arno@justyo.co>
2015-03-16 11:04:48 -0700

    Initial commit, setup the primary functionality.

---
+++ public/src/js/app.js
@@ -0,0 +1,11 @@
+/** @jsx React.DOM */
+
+var App = React.createClass({
+  render : function() {
+    return (
+     React.createElement("h1", null, 'Hello World!')
+    )
+  }
+});
+
+module.exports = App;
